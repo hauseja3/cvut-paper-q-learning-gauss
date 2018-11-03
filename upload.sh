b5e41cc3dde2e253150fb830eac894db913e23a3
Jan Hauser <hauseja3@gmail.com>
2018-11-03 13:32:59 +0100

    fix upload script

--- upload.sh
+++ upload.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# export to pdf
-pdflatex ecc19.tex
+# three parameters:
+# $1 - boolean if pdf should be exported from ecc19.tex file 
+# $2 - origin branch in which should be pushed (master)
+# $3 - commit message
+# example usage bash upload.sh true master "this is some commit message"
+
+if $1; then
+	# export to pdf
+	pdflatex ecc19.tex
+fi
 
 # push to github 
 git add .
-git commit -m "$2"
-git push -u  origin $1
+git commit -m "$3"
+git push -u  origin $2
